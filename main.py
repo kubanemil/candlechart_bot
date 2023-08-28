12b1d11139b8e7a71595e82a4a0fe502fa6955a0
Emil <emil3toktobekov@gmail.com>
2023-08-28 19:29:49 +0600

    PEP8

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-from tools import get_data, create_plot
+from tools import get_data, get_ema, convert_to_ohlc, create_plot
 
-# fn = input('')
-df = get_data('prices.csv')
+# Inputs
+fn = input('1. Enter a data\'s file name ("ENTER" for default="prices.csv"): ')
 
-candlestick_interval = '12H'
-# display_interval = (datetime.date())
+print('\n2. Enter an interval for candlestick (ex. 2 days => 2D or 10 hours = > 10H or 15 mintues => 15T')
+candlestick_interval = input('("ENTER" for (default="12H")): ') or '12H'
 
-candle_data = df.resample(candlestick_interval).apply({
-    "price": ["first", "max", "min", "last"]
-})
+ema_period = int(input(f'\n3. Enter a period to calculate EMA (default_period=3): ') or 3)
 
-candle_data.columns = ["open", "high", "low", "close"]
-ema_period = 2
-max_period = len(candle_data)
-if ema_period > max_period:
-    raise ValueError(f'Period is more than allowed! Your period: {ema_period} (Max period: {max_period})')
+print('\n4. Enter which period to display in percents (e.g. 10-65 will show a date from 10% to 65%')
+display_period = (input('("ENTER" to show for all period): ') or '0-100').split('-')
 
-create_plot(candle_data)
+df = get_data('prices.csv', start=int(display_period[0]), end=int(display_period[1]))
+candle_data = convert_to_ohlc(df, interval=candlestick_interval)
+ema = get_ema(candle_data, period=ema_period)
+# Final Plot
+create_plot(candle_data, ema=ema)
--- tools.py
+++ tools.py
@@ -2,16 +2,30 @@ import mplfinance as mpf
 import pandas as pd
 
 
-def get_data(fn='prices.csv'):
+def get_data(fn='prices.csv', start=0, end=100):
     df = pd.read_csv('prices.csv')
+    df_len = len(df)
+    df = df[int(df_len*start/100): int(df_len*end/100)]
     df["TS"] = pd.to_datetime(df["TS"])
     df.columns = ['timestamp', 'price']
     df.set_index("timestamp", inplace=True)
     return df
 
 
-def create_plot(data, ema_period=3):
-    ema = data['close'].ewm(span=ema_period, adjust=False).mean()
+def convert_to_ohlc(df, interval='1D'):
+    candle_data = df.resample(interval).apply({
+        "price": ["first", "max", "min", "last"]
+    })
 
+    candle_data.columns = ["open", "high", "low", "close"]
+    return candle_data
+
+
+def get_ema(data, period):
+    ema = data['close'].ewm(span=period, adjust=False).mean()
+    return ema
+
+
+def create_plot(data, ema):
     mpf.plot(data, type='candle', style='charles', title="Candlestick Chart with EMA",
-             addplot=[mpf.make_addplot(ema, color='orange')], ylabel='Price')
+             addplot=[mpf.make_addplot(ema, color='orange')], ylabel='Price', xlabel='Date')
